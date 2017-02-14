f9a96687361186f32b081f02b9c30aa821e34571
Rellika Kisyula <kisyular@msu.edu>
2017-02-13 20:58:11 -0500

    Modified onclick function to Event Listener and fixed the identation issue

--- js/script.js
+++ js/script.js
@@ -2,18 +2,18 @@ var theme = window.localStorage.getItem('getTheme')
 window.onload = function() {
   document.body.setAttribute('class', theme)
   if (localStorage.pagecount) {
-    localStorage.pagecount = Number(localStorage.pagecount) + 1;
+    localStorage.pagecount = Number(localStorage.pagecount) + 1
   } else {
-    localStorage.pagecount = 1;
+    localStorage.pagecount = 1
   }
-  document.getElementById('numVisitors').innerHTML = "You visited this website: " + '<strong id="red">' + localStorage.pagecount + '</strong>' + " time(s)."
-  document.getElementById('red').style.color = 'red';
+  document.getElementById('numVisitors').innerHTML = 'You visited this website: ' + '<strong id="red">' + localStorage.pagecount + '</strong>' + ' time(s).'
+  document.getElementById('red').style.color = 'red'
 }
 $('#myModal').modal({
   show: false
 })
 if (theme === null) {
-  show_selected()
+  showSelected()
   if (theme === null) {
     theme = 'night-theme'
   } else {
@@ -21,7 +21,13 @@ if (theme === null) {
   }
 }
 
-function changeTheme() {
+var clearStorage = document.getElementById('clearStorage')
+clearStorage.addEventListener('click', function() {
+  localStorage.clear()
+})
+
+var buttonElement = document.getElementById('changeTm')
+buttonElement.addEventListener('click', function() {
   if (document.getElementById('myBody').className === 'night-theme' || document.getElementById('myBody').className === '') {
     document.body.setAttribute('class', 'day-theme')
     window.localStorage.setItem('getTheme', 'day-theme')
@@ -29,14 +35,14 @@ function changeTheme() {
     document.body.setAttribute('class', 'night-theme')
     window.localStorage.setItem('getTheme', 'night-theme')
   }
-}
+})
 
-document.getElementById('saveTheme').addEventListener('click', show_selected);
+document.getElementById('saveTheme').addEventListener('click', showSelected)
 
-function show_selected() {
-  $('#myModal').modal('show');
-  var selector = document.getElementById('themeOptions');
-  var theme = selector[selector.selectedIndex].value;
+function showSelected() {
+  $('#myModal').modal('show')
+  var selector = document.getElementById('themeOptions')
+  var theme = selector[selector.selectedIndex].value
   window.localStorage.setItem('getTheme', theme)
   document.body.setAttribute('class', theme)
 }
